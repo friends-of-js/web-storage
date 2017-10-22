8efa6e740da215232dc17ba48ed7773e9d977ff4
noldors <romanov@noldor.pro>
2017-10-23 02:47:07 +0300

    test(travis): Add travis for testing

--- build/karma.config.ts
+++ build/karma.config.ts
@@ -1,91 +1,95 @@
 import webpackConfig from './testing'
 import * as path from 'path'
 
-export default (config: any) => {
-  config.set({
-    basePath: '../',
-    failOnEmptyTestSuite: true,
-    frameworks: ['mocha', 'chai'],
+const configuration = {
+  basePath: '../',
+  failOnEmptyTestSuite: true,
+  frameworks: ['mocha', 'chai'],
 
-    files: [
-      'spec/index.ts',
-      'src/index.ts',
-      { pattern: 'spec/**/*.spec.ts', watched: true }
-    ],
+  files: [
+    'spec/index.ts',
+    'src/index.ts',
+    { pattern: 'spec/**/*.spec.ts', watched: true }
+  ],
 
-    mime: {
-      'text/x-typescript': ['ts', 'tsx']
+  mime: {
+    'text/x-typescript': ['ts', 'tsx']
+  },
+
+  plugins: [
+    require('karma-webpack'),
+    require('karma-mocha'),
+    require('karma-coverage-istanbul-reporter'),
+    require('karma-coverage'),
+    require('karma-chai'),
+    require('karma-mocha-reporter'),
+    require('karma-notify-reporter'),
+    require('karma-chrome-launcher'),
+    require('karma-firefox-launcher')
+  ],
+
+  exclude: [],
+
+  webpack: webpackConfig,
+
+  webpackMiddleware: {
+    noInfo: true,
+    quiet: true,
+    stats: {
+      colors: true
     },
+    publicPath: '/'
+  },
 
-    plugins: [
-      require('karma-webpack'),
-      require('karma-mocha'),
-      require('karma-coverage-istanbul-reporter'),
-      require('karma-coverage'),
-      require('karma-chai'),
-      require('karma-mocha-reporter'),
-      require('karma-notify-reporter'),
-      require('karma-chrome-launcher'),
-      require('karma-firefox-launcher')
-    ],
+  preprocessors: {
+    'src/**/*.ts': ['webpack'],
+    'spec/*.ts': ['webpack']
+  },
 
-    exclude: [],
+  reporters: ['mocha', 'coverage-istanbul', 'notify'],
 
-    webpack: webpackConfig,
-
-    webpackMiddleware: {
-      noInfo: true,
-      quiet: true,
-      stats: {
-        colors: true
+  coverageIstanbulReporter: {
+    dir: path.resolve(__dirname, '../coverage'),
+    reports: ['html', 'lcov'],
+    fixWebpackSourcePaths: false,
+    skipFilesWithNoCoverage: true,
+    'report-config': {
+      html: {
+        subdir: 'html'
       },
-      publicPath: '/'
-    },
-
-    preprocessors: {
-      'src/**/*.ts': ['webpack'],
-      'spec/*.ts': ['webpack']
-    },
-
-    reporters: ['mocha', 'coverage-istanbul', 'notify'],
-
-    coverageIstanbulReporter: {
-      dir: path.resolve(__dirname, '../coverage'),
-      reports: ['html', 'lcov'],
-      fixWebpackSourcePaths: false,
-      skipFilesWithNoCoverage: true,
-      'report-config': {
-        html: {
-          subdir: 'html'
-        },
-        lcov: {
-          subdir: 'lcov'
-        }
+      lcov: {
+        subdir: 'lcov'
       }
-    },
+    }
+  },
 
-    port: 9876,
+  port: 9876,
 
-    client: {
-      captureConsole: true,
-      clearContext: true
-    },
+  client: {
+    captureConsole: true,
+    clearContext: true
+  },
 
-    logLevel: config.LOG_INFO,
+  colors: true,
 
-    colors: true,
+  autoWatch: true,
 
-    autoWatch: true,
+  browsers: ['Chrome', 'Firefox'],
 
-    browsers: ['Chrome', 'Firefox'],
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+  },
 
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+  concurrency: Infinity
+}
 
-    concurrency: Infinity
-  })
+if (process.env.TRAVIS) {
+  configuration.browsers = ['Chrome_travis_ci']
+}
+
+export default (config: any) => {
+  config.set(configuration)
 }
